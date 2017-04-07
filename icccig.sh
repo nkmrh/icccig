f428f91fb12c9312e746316d306a52e8bde5cb2b
shintaro-morikawa <sntr92@gmail.com>
2017-04-07 13:28:53 +0900

    Improve script

--- icccig.sh
+++ icccig.sh
@@ -9,13 +9,12 @@ BORDER='2x2'
 
 mkdir -p $EXPORT_DIR
 
-while read code; do
+while read -r code; do
   images=()
-  for i in `seq 1 ${#code}`
-  do
-	images+=("${FONT_DIR}/${${code:$i-1:1}}.jpg")
+  for i in $(seq 1 ${#code}); do
+    images+=("${FONT_DIR}/${code:$i-1:1}.jpg")
   done
-  montage $images -tile ${#code}x1 -geometry +0+0 $TEMP
+  convert +append "${images[@]}" $TEMP
   convert $TEMP -background white -gravity northwest -splice $MARGIN $TEMP
   convert $TEMP -background white -gravity southeast -splice $MARGIN $TEMP
   convert $TEMP -bordercolor black -border $BORDER "${EXPORT_DIR}/${code}.jpg"
